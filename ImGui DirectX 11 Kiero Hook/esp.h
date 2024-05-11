c2dc72cfee380daf7aba9d76cea7131636e493fe
fluffysnaff <fluffysnaff+gitlab@protonmail.com>
2024-05-11 16:57:48 +0200

    Added GetPlayerHeight()

--- ImGui DirectX 11 Kiero Hook/esp.h
+++ ImGui DirectX 11 Kiero Hook/esp.h
@@ -10,5 +10,12 @@ private:
 	void EspPolice(SDK::AActor* currActor);
 
 	// Base
+	float GetPlayerHeight(ImVec2 head, ImVec2 feet)
+	{
+		const float temp1 = powf(head.x - feet.x, 2.f);
+		const float temp2 = powf(head.y - feet.y, 2.f);
+		const float height = sqrtf(temp1 + temp2);
+		return height;
+	}
 	void RenderNameplate(SDK::FVector2D pos, std::string name);
 };
