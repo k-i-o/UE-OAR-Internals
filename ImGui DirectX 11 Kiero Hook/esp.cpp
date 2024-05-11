3abd253c52ac6d43c0d1c798dea0e6c42b2c8f77
fluffysnaff <fluffysnaff+gitlab@protonmail.com>
2024-05-11 16:59:23 +0200

    Added esp_base.cpp

--- ImGui DirectX 11 Kiero Hook/esp.cpp
+++ ImGui DirectX 11 Kiero Hook/esp.cpp
@@ -72,28 +72,3 @@ void KFNEsp::EspPolice(SDK::AActor* currActor)
 	RenderNameplate(footPos, currActor->GetName());
 	RenderBox(headPos, footPos);
 }
-
-
-// Base
-void KFNEsp::RenderNameplate(SDK::FVector2D pos, std::string name)
-{
-	// Centered nameplate
-	pos.X -= ImGui::CalcTextSize(name.c_str()).x * 0.5f;
-	ImGui::GetBackgroundDrawList()->AddText(ImVec2{ pos.X, pos.Y }, IM_COL32(255, 0, 0, 255), name.c_str());
-}
-
-void KFNEsp::RenderBox(SDK::FVector2D headPos, SDK::FVector2D footPos)
-{
-	const float height = GetPlayerHeight(ImVec2{ headPos.X, headPos.Y }, ImVec2{ footPos.X, footPos.Y });
-	const float width = height * 0.5f;
-
-	const ImVec2 upperLeft = { footPos.X - width * 0.5f, footPos.Y - height };
-	const ImVec2 lowerRight = { footPos.X + width * 0.5f, footPos.Y };
-	ImGui::GetBackgroundDrawList()->AddRect(upperLeft,
-		lowerRight,
-		IM_COL32(0, 0, 0, 225.f),
-		0.f,
-		ImDrawCornerFlags_All,
-		2.5f);
-	ImGui::GetBackgroundDrawList()->AddRect(upperLeft, lowerRight, IM_COL32(255, 0, 0, 255), 0.f, ImDrawCornerFlags_All, 1.5f);
-}
--- ImGui DirectX 11 Kiero Hook/esp.h
+++ ImGui DirectX 11 Kiero Hook/esp.h
@@ -10,14 +10,7 @@ private:
 	void EspPolice(SDK::AActor* currActor);
 
 	// Base
-	float GetPlayerHeight(ImVec2 head, ImVec2 feet)
-	{
-		const float temp1 = powf(head.x - feet.x, 2.f);
-		const float temp2 = powf(head.y - feet.y, 2.f);
-		const float height = sqrtf(temp1 + temp2);
-		return height;
-	}
-
+	float GetPlayerHeight(ImVec2 head, ImVec2 feet);
 	void RenderNameplate(SDK::FVector2D pos, std::string name);
 	void RenderBox(SDK::FVector2D headPos, SDK::FVector2D footPos);
 };
