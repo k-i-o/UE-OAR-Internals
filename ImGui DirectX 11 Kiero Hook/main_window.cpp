11d230430f0cc18008ebb3acd1caa37b2702681f
fluffysnaff <fluffysnaff+gitlab@protonmail.com>
2024-05-13 19:45:48 +0200

    Added Host tabs + removed exploits tab

--- ImGui DirectX 11 Kiero Hook/main_window.cpp
+++ ImGui DirectX 11 Kiero Hook/main_window.cpp
@@ -76,10 +76,6 @@ void KFNGUI::RenderMainWindow()
 			{
 				ImGui::Indent();
 
-				ImGui::Checkbox("Guard phone time", &manager->m_pConfig->miscHacks.addedGuardPhoneTime);
-				if (manager->m_pConfig->miscHacks.addedGuardPhoneTime)
-					ImGui::SliderInt("##timeGuardPhone", &manager->m_pConfig->miscHacks.phoneTime, 0, 999);
-
 				ImGui::Checkbox("Drill immunity time", &manager->m_pConfig->miscHacks.addedDrillImmunityTime);
 				if (manager->m_pConfig->miscHacks.addedDrillImmunityTime)
 					ImGui::SliderInt("##timeImmunityDrill", &manager->m_pConfig->miscHacks.drillImmunityTime, 0, 999);
@@ -127,11 +123,16 @@ void KFNGUI::RenderMainWindow()
 			ImGui::EndTabItem();
 		}
 
-		if(ImGui::BeginTabItem("Exploits"))
+		if(ImGui::BeginTabItem("Host"))
 		{
 			ImGui::PushStyleColor(ImGuiCol_Text, IM_COL32(255, 204, 0, 255));
-			ImGui::Text("Warning! Doesn't work on these maps: Orbital Bank, Black Diamond Casino, Esco Bar");
+			ImGui::Text("Warning! Might not work on these maps: Orbital Bank, Black Diamond Casino, Esco Bar");
 			ImGui::PopStyleColor();
+
+			ImGui::Checkbox("Guard phone time", &manager->m_pConfig->miscHacks.addedGuardPhoneTime);
+			if (manager->m_pConfig->miscHacks.addedGuardPhoneTime)
+				ImGui::SliderInt("##timeGuardPhone", &manager->m_pConfig->miscHacks.phoneTime, 0, 999);
+
 			if (ImGui::Button("Kill civilians"))
 				manager->m_pConfig->teleportExploits.killCivilians = true;
 
